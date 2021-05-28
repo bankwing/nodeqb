fd565bc2ff974731a444b7492c156dbe7d8b140c
prasanth <prasanth.react@gmail.com>
2021-05-28 21:47:02 +0530

    query error added

--- src/helper/basicHelper.ts
+++ src/helper/basicHelper.ts
@@ -1,7 +1,7 @@
 export const devConsoleLog = (a: any, b: any = '') => {
-  console.log(a, b);
+    console.log(a, b);
 };
 
 export const spaceRemover = (str: string) => {
-  return str.replace(/\s+/g, ' ');
+    return str.replace(/\s+/g, ' ');
 };
--- src/types/typeInterface.ts
+++ src/types/typeInterface.ts
@@ -1,181 +1,181 @@
 import NodeQB from '../index';
-import { ConnectionConfig, MysqlError, PoolConfig, queryCallback } from 'mysql';
+import {ConnectionConfig, MysqlError, PoolConfig, queryCallback} from 'mysql';
 import MysqlConnection from '../database/mysql';
 
 export interface NodeQBInterface {
-  table(str: string): NodeQB;
+    table(str: string): NodeQB;
 
-  get(callback?: queryCallback): unknown;
+    get(callback?: queryCallback): unknown;
 
-  value(str: string): unknown;
+    value(str: string): unknown;
 
-  find(str: string): NodeQB;
+    find(str: string): NodeQB;
 
-  //aggregation method
-  get(callback?: queryCallback): unknown;
+    //aggregation method
+    get(callback?: queryCallback): unknown;
 
-  max(str: string): NodeQB;
+    max(str: string): NodeQB;
 
-  avg(str: string): NodeQB;
+    avg(str: string): NodeQB;
 
-  exists(str: string): NodeQB;
+    exists(str: string): NodeQB;
 
-  doesntExist(str: string): NodeQB;
+    doesntExist(str: string): NodeQB;
 
-  //select statement
-  select(str: string): NodeQB;
+    //select statement
+    select(str: string): NodeQB;
 
-  distinct(str: string): NodeQB;
+    distinct(str: string): NodeQB;
 
-  addSelect(str: string): NodeQB;
+    addSelect(str: string): NodeQB;
 
-  //raw method
-  selectRaw(str: string): NodeQB;
+    //raw method
+    selectRaw(str: string): NodeQB;
 
-  whereRaw(str: string): NodeQB;
+    whereRaw(str: string): NodeQB;
 
-  havingRaw(str: string): NodeQB;
+    havingRaw(str: string): NodeQB;
 
-  groupByRaw(str: string): NodeQB;
+    groupByRaw(str: string): NodeQB;
 
-  orderByRaw(str: string): NodeQB;
+    orderByRaw(str: string): NodeQB;
 
-  //joins
-  join(str: string): NodeQB;
+    //joins
+    join(str: string): NodeQB;
 
-  leftJoin(str: string): NodeQB;
+    leftJoin(str: string): NodeQB;
 
-  righJoin(str: string): NodeQB;
+    righJoin(str: string): NodeQB;
 
-  crossJoin(str: string): NodeQB;
+    crossJoin(str: string): NodeQB;
 
-  //advance joins
-  orOn(str: string): NodeQB;
+    //advance joins
+    orOn(str: string): NodeQB;
 
-  //union
-  union(str: string): NodeQB;
+    //union
+    union(str: string): NodeQB;
 
-  //where clauses
-  where(str: string): NodeQB;
+    //where clauses
+    where(str: string): NodeQB;
 
-  orWhere(str: string): NodeQB;
+    orWhere(str: string): NodeQB;
 
-  whereNull(str: string): NodeQB;
+    whereNull(str: string): NodeQB;
 
-  whereNotNull(str: string): NodeQB;
+    whereNotNull(str: string): NodeQB;
 
-  whereYear(str: string): NodeQB;
+    whereYear(str: string): NodeQB;
 
-  whereMonth(str: string): NodeQB;
+    whereMonth(str: string): NodeQB;
 
-  whereDate(str: string): NodeQB;
+    whereDate(str: string): NodeQB;
 
-  whereDay(str: string): NodeQB;
+    whereDay(str: string): NodeQB;
 
-  whereTime(str: string): NodeQB;
+    whereTime(str: string): NodeQB;
 
-  whereColumn(str: string): NodeQB;
+    whereColumn(str: string): NodeQB;
 
-  whereExists(str: string): NodeQB;
+    whereExists(str: string): NodeQB;
 
-  //Ordering, Grouping, Limit & Offset
-  orderBy(str: string): NodeQB;
+    //Ordering, Grouping, Limit & Offset
+    orderBy(str: string): NodeQB;
 
-  orderByAsc(str: string): NodeQB;
+    orderByAsc(str: string): NodeQB;
 
-  orderByDesc(str: string): NodeQB;
+    orderByDesc(str: string): NodeQB;
 
-  inRandomOrder(str: string): NodeQB;
+    inRandomOrder(str: string): NodeQB;
 
-  reorder(str: string): NodeQB;
+    reorder(str: string): NodeQB;
 
-  latest(str: string): NodeQB;
+    latest(str: string): NodeQB;
 
-  oldest(str: string): NodeQB;
+    oldest(str: string): NodeQB;
 
-  first(str: string): NodeQB;
+    first(str: string): NodeQB;
 
-  last(str: string): NodeQB;
+    last(str: string): NodeQB;
 
-  //Grouping having
-  groupBy(str: string): NodeQB;
+    //Grouping having
+    groupBy(str: string): NodeQB;
 
-  having(str: string): NodeQB;
+    having(str: string): NodeQB;
 
-  //limit and offset
-  skip(str: string): NodeQB;
+    //limit and offset
+    skip(str: string): NodeQB;
 
-  take(str: string): NodeQB;
+    take(str: string): NodeQB;
 
-  offset(str: string): NodeQB;
+    offset(str: string): NodeQB;
 
-  limit(str: string): NodeQB;
+    limit(str: string): NodeQB;
 
-  //conditional
-  ifNull(str: string): NodeQB;
+    //conditional
+    ifNull(str: string): NodeQB;
 
-  //insert or update
-  insert(str: string): NodeQB;
+    //insert or update
+    insert(str: string): NodeQB;
 
-  update(str: string): NodeQB;
+    update(str: string): NodeQB;
 
-  insertGetId(str: string): NodeQB;
+    insertGetId(str: string): NodeQB;
 
-  insertOrUpdate(str: string): NodeQB;
+    insertOrUpdate(str: string): NodeQB;
 
-  updateOrInsert(str: string): NodeQB;
+    updateOrInsert(str: string): NodeQB;
 
-  increment(str: string): NodeQB;
+    increment(str: string): NodeQB;
 
-  decrement(str: string): NodeQB;
+    decrement(str: string): NodeQB;
 
-  //Pessimistic Locking
-  sharedLock(str: string): NodeQB;
+    //Pessimistic Locking
+    sharedLock(str: string): NodeQB;
 
-  //debug
-  dd(str: string): NodeQB;
+    //debug
+    dd(str: string): NodeQB;
 }
 
 export type method = 'pool' | 'default';
 
 export namespace NodeQBConnectionInterface {
-  export interface Constructor {
-    type: keyof database;
-    config: config;
-    prevent?: boolean;
-    defaults?: defaults;
-    method?: method;
-  }
+    export interface Constructor {
+        type: keyof database;
+        config: config;
+        prevent?: boolean;
+        defaults?: defaults;
+        method?: method;
+    }
 
-  export interface database {
-    mysql: MysqlConnectionInterface;
-  }
+    export interface database {
+        mysql: MysqlConnectionInterface;
+    }
 
-  export interface defaults {
-    orderColumn?: string | Array<string>;
-  }
+    export interface defaults {
+        orderColumn?: string | Array<string>;
+    }
 
-  export interface Join {
-    (joinTable: string, callbackOrId: any, condition?: string, secondaryId?: string, mode?: Joins): NodeQB;
-  }
+    export interface Join {
+        (joinTable: string, callbackOrId: any, condition?: string, secondaryId?: string, mode?: Joins): NodeQB;
+    }
 }
 export type config = ConnectionConfig & PoolConfig;
 export type Joins = 'INNER JOIN' | 'LEFT JOIN' | 'RIGHT JOIN';
 
 export interface MysqlConnectionInterface {
-  getInstance(callback?: (err: MysqlError) => void): MysqlConnection;
+    getInstance(callback?: (err: MysqlError) => void): MysqlConnection;
 }
 
 export type ReturnModeTypes = 'single' | 'default' | 'insert';
 
 export type ReturnModeObjInterface = {
-  [key in ReturnModeTypes]: keyof MysqlConnection;
+    [key in ReturnModeTypes]: keyof MysqlConnection;
 };
 
 export interface ExecInterface {
-  returnMode?: ReturnModeTypes;
-  callback?: queryCallback;
-  value?: string;
+    returnMode?: ReturnModeTypes;
+    callback?: queryCallback;
+    value?: string;
 }
 
 export type Condition = 'AND' | 'OR';
